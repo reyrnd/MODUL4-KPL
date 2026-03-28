e3188d0ba4ef21e6395a73ad511b4f4782a623ec
rerxyz_ <reynard16rande@gmail.com>
2026-03-28 14:08:58 +0700

    second commit

--- Modul4_103022400125/Program.cs
+++ Modul4_103022400125/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class KodePaket
+{
+    public (string[] namaPaket, string[] kodePaket) getKodePaket()
+    {
+        string[] namaPaket = { "Basic", "Standard", "Premium", "Unlimited", "Gaming", "Streaming", "Family", "Business", "Student", "Traveler" };
+
+        string[] kodePaket = { "P201", "P202", "P203", "P204", "P205", "P206", "P207", "P208", "P209", "P210" };
+
+        return (namaPaket, kodePaket);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        KodePaket kodes = new KodePaket();
+        var (namaPaket, kodePaket) = kodes.getKodePaket();
+        Console.WriteLine("Daftar nama paket dan kode paket");
+
+        for (int i = 0; i < namaPaket.Length; i++)
+        {
+            Console.WriteLine("Kelurahan " + namaPaket[i] + " : " + kodePaket[i]);
+        }
+
+
+    }
+}
